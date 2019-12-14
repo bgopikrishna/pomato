a94a5972bdb56370f9e38f39c20ef8f2495d5b34
Gopi Krishna <gopiwayz@gmail.com>
2019-12-14 20:41:43 +0530

    underhood changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  ZOMATO_API_KEY: "141f2f0600ff1ce5ac55411a1f2c16e5",
-  MAPS_KEY:"AIzaSyCjLjUcPmkq5G13-QZ2Ro7ugoDJNbTOVnA"
+  ZOMATO_API_KEY: "cb1d1cfb2b169f1bd6dbae4d52a55d46",
 };
 
 /*
